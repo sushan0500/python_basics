deba75db375e2c63d9d739acc02780c20b851aa4
sushan0500 <4al23ai058@aiet.org.in>
2026-06-25 16:52:37 +0530

    python_basics

---
+++ test.py
@@ -0,0 +1,23 @@
+# print("hello world","world",sep='#')
+# print("hello world","world",end="")
+# print("hello world")        
+# print("10.5"*2)
+# print(int(float(x)))
+# y= 's'
+# print("abc">"abd")
+# x = int(input(""))
+# print("\test\ntesting")
+# print(r"\test\ntesting")
+# print("\\test\\ntesting")
+# print("""helllo
+#      """)
+# x="hello"
+# print(x[0:3:2])
+# print(x[::-1])
+# while(True):
+#    print("hello world")
+# for i in range(1,10):
+#    if i%2==0:
+#        print(i)
+for i in range(0,10,2):
+        print(i)
